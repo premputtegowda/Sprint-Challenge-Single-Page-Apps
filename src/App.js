8af38de948e742af8d47ad0d55ed53ff16af7895
Prem Puttegowda <puttegowdaprem@gmail.com>
2019-12-20 08:32:53 -0800

    implemented routes to character and home pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage"
+import { Route } from "react-router-dom"
 
 
 export default function App() {
@@ -8,7 +10,14 @@ export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+      
+
+      <Route exact path="/">
+          <WelcomePage />
+      </Route>
+      <Route path="/characters">
+        <CharacterList />
+      </Route>
     </main>
   );
 }
