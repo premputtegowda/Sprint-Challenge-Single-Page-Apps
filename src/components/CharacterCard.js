b6ddf45e3555ae851152e9114bf68dad0e5e53bd
Prem Puttegowda <puttegowdaprem@gmail.com>
2019-12-20 08:22:18 -0800

    Added api and created charactercard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({character}) {
+  return (
+    <div>
+      <h3>{character.name}</h3>
+      <p>Status: {character.status}</p>
+    </div>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import CharacterCard from "./CharacterCard"
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([])
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
+    axios.get("https://rickandmortyapi.com/api/character")
+    .then(response => {
+      console.log(response.data.results)
+      setCharacters(response.data.results)
+    })
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>Characters</h2>
+      {characters.map(character=> {
+        return <CharacterCard key={character.id} character={character}/>
+      })}
     </section>
   );
 }
