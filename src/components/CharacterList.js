20d9b3ae9198bc157a9d5e8392faa71b4b03edff
Prem Puttegowda <puttegowdaprem@gmail.com>
2019-12-20 09:36:05 -0800

    implemented search functionality

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,24 +1,39 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import CharacterCard from "./CharacterCard"
-
+import SearchForm from "./SearchForm"
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState([])
+  const [query, setQuery] = useState("")
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     axios.get("https://rickandmortyapi.com/api/character")
     .then(response => {
       console.log(response.data.results)
-      setCharacters(response.data.results)
+      
+      const data = response.data.results.filter(character => 
+        character.name
+        .toLowerCase()
+        .includes(query.toLowerCase()) 
+      )
+     
+    
+      setCharacters(data)
+    
+    
+
+      
     })
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+  }, [query]);
 
   return (
+    
     <section className="character-list">
+      <SearchForm query={query} setQuery={setQuery}/>
       <h2>Characters</h2>
       {characters.map(character=> {
         return <CharacterCard key={character.id} character={character}/>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
+import { tsPropertySignature } from "@babel/types";
 
-export default function SearchForm() {
+export default function SearchForm(props) {
+ 
+ const {query} = props;
+ const {setQuery} = props;
+ const handleInputchange = (event)=> {
+  console.log("search term: ",event.target.value) 
+  setQuery(event.target.value);
+   
+  }
+  // useEffect(()=>{
+  //   const result = characters.filter(character => {
+  //     return character.name.toLowerCase().includes(query.toLowerCase()) 
+  //   })
+  //   console.log(result);
+  //   if (result){
+  //     setCharacters(result)
+  //   }
+    
+  // },[query])
+  
+ 
+ 
  
   return (
     <section className="search-form">
-     // Add a search form here
+   
+     <form className="search">
+       <input type="text"
+       name="name"
+       placeholder="search character"
+       onChange={handleInputchange}
+       value={query}/>
+     </form>
+
     </section>
   );
 }
